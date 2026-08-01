d94ce7c959012aa7019d65144e0c57a9f8f139b9
Kufik <127012125+Onmaynec@users.noreply.github.com>
2026-08-01 13:35:57 +0300

    Добавить метаданные продукта 0.7.0

---
+++ src/SysDiff.Cli/ProductInfo.cs
@@ -0,0 +1,13 @@
+namespace SysDiff.Cli;
+
+public static class ProductInfo
+{
+    public const string Name = "SysDiff";
+    public const string Version = "0.7.0";
+    public const string Runtime = "win-x64";
+    public const string Repository = "Onmaynec/SysDiff";
+    public const string StableManifestUrl =
+        "https://github.com/Onmaynec/SysDiff/releases/latest/download/release-manifest.json";
+
+    public static ReleaseVersion ParsedVersion => ReleaseVersion.Parse(Version);
+}
