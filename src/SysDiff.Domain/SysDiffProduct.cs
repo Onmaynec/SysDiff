eea9589666f2b507644b2afc5bf2500c56c58005
Kufik <127012125+Onmaynec@users.noreply.github.com>
2026-08-02 11:11:08 +0300

    Централизовать версию продукта и schema contract

---
+++ src/SysDiff.Domain/SysDiffProduct.cs
@@ -0,0 +1,12 @@
+namespace SysDiff.Domain;
+
+public static class SysDiffProduct
+{
+    public const string Version = "0.10.0";
+
+    public const int PublicSchemaVersion = 1;
+
+    public const int SnapshotArchiveFormatVersion = 1;
+
+    public const int InvestigationBundleFormatVersion = 1;
+}
